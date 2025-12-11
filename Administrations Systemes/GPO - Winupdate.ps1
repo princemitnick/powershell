0e771b02a14073a95f050464a41953c9f1f61495
PSLJJ <ingjeanbaptiste@gmail.com>
2025-12-10 20:00:28 -0500

    PS scripting module 1

--- Administrations Systemes/GPO - Winupdate.ps1
+++ Administrations Systemes/GPO - Winupdate.ps1
@@ -5,3 +5,4 @@ Get-ADComputer -Filter * | Select-Object Name, OperatingSystem
 Get-WmiObject Win32_PnPEntity | Where-Object { $_.PNPClass -eq "Display" } | Select-Object Name, DeviceID
 
 
+Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "Lenovo System Update*" } | ForEach-Object { $_.Uninstall() }
---
+++ Administrations Systemes/uninstalllenovoupdate.ps1
@@ -0,0 +1,15 @@
+$services = @(
+    "ImControllerService",
+    "Lenovo Vantage Service",
+    "LSUService"
+)
+
+foreach ($s in $services) {
+    if (Get-Service -Name $s -ErrorAction SilentlyContinue) {
+        Stop-Service $s -Force -ErrorAction SilentlyContinue
+        Set-Service $s -StartupType Disabled
+        Write-Host "$s désactivé."
+    } else {
+        Write-Host "$s non trouvé."
+    }
+}
